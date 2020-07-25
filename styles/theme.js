1f3d1152a5a46c3be82fbbd63263f4a614ea2254
Yanto Lee <yanto3528@gmail.com>
2020-07-25 09:46:54 +0700

    initial commit

---
+++ styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+  primary: "#2acfcf",
+  secondary: "#3b3054",
+  tertiary: "	#f46262",
+  grey: "#bfbfbf",
+  grey2: "#9e9aa7",
+  darkgrey: "	#35323e",
+  darkgrey2: "#232127",
+};
